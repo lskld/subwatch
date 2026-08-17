e0f98ed64f390de0db1bd4c8c4fbbd1a61d2588a
lskld <theo.lillieskold@gmail.com>
2026-08-17 12:01:22 +0200

    set up empty entities including identity user

---
+++ subwatch-api/Models/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace SubwatchApi.Models.Entites
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
---
+++ subwatch-api/Models/Entities/PriceHistory.cs
@@ -0,0 +1,7 @@
+namespace SubwatchApi.Models.Entites
+{
+    public class PriceHistory
+    {
+        
+    }
+}

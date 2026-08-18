1d9ca07027baab41bb67607b55f732bbc01cd3d3
lskld <theo.lillieskold@gmail.com>
2026-08-18 13:00:51 +0200

    changed return type to authresult dto and implemented the RegisterAsync method

--- subwatch-api/Services/AuthService.cs
+++ subwatch-api/Services/AuthService.cs
@@ -1,16 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 using SubwatchApi.Models.DTOs;
+using SubwatchApi.Models.Entities;
 
 namespace SubwatchApi.Services
 {
-    public class AuthService : IAuthService
+    public class AuthService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ITokenService tokenService) : IAuthService
     {
-        public Task<string> LoginAsync(LoginDto dto)
+        public async Task<AuthResult> LoginAsync(LoginDto dto)
         {
             throw new NotImplementedException();
         }
-        public Task<string> RegisterAsync(RegisterDto dto)
+        public async Task<AuthResult> RegisterAsync(RegisterDto dto)
         {
-            throw new NotImplementedException();
+            var user = new ApplicationUser
+            {
+                UserName = dto.Username,
+                Email = dto.Email,
+            };
+            var result = await userManager.CreateAsync(user, dto.Password);
+
+            if (!result.Succeeded)
+                return AuthResult.Failure(result.Errors.Select(e => e.Description));
+
+            return AuthResult.Success(tokenService.GenerateJwtToken(user));
         }
     }
 }

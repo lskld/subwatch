dc29f6901e9d26ea86db40add63fc67826ec0ea8
lskld <theo.lillieskold@gmail.com>
2026-08-19 11:28:52 +0200

    same as last commit

--- subwatch-api/Services/SubscriptionCategoryService.cs
+++ subwatch-api/Services/SubscriptionCategoryService.cs
@@ -1,7 +1,19 @@
+using SubwatchApi.Data;
+using SubwatchApi.Models.DTOs;
+using SubwatchApi.Models.Entities;
+
 namespace SubwatchApi.Services
 {
-    public class SubscriptionCategoryService : ISubscriptionCategoryService
+    public class SubscriptionCategoryService(SubwatchDbContext dbContext) : ISubscriptionCategoryService
     {
-        
+        public async Task<CreateSubscriptionCategoryResponse> CreateAsync(CreateSubscriptionCategoryRequest req, string userId)
+        {
+            var newCategory = new SubscriptionCategory { Title = req.Title, Description = req.Description, UserId = userId };
+
+            dbContext.SubscriptionCategories.Add(newCategory);
+            await dbContext.SaveChangesAsync();
+
+            return new CreateSubscriptionCategoryResponse(newCategory.Id, newCategory.Title, newCategory.Description);
+        }
     }
 }

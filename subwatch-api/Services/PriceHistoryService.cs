7bb8a7950380534ffc2a24937d7e18a567a57d8a
lskld <theo.lillieskold@gmail.com>
2026-08-20 15:21:47 +0200

    implement all empty methods from interface as not implemented

--- subwatch-api/Services/PriceHistoryService.cs
+++ subwatch-api/Services/PriceHistoryService.cs
@@ -1,7 +1,28 @@
+using SubwatchApi.Models.DTOs;
+
 namespace SubwatchApi.Services
 {
     public class PriceHistoryService : IPriceHistoryService
     {
-        
+        public Task<PriceHistoryResponse> CreateAsync(CreatePriceHistoryRequest req, string userId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<PriceHistoryResponse?> GetByIdAsync(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<List<PriceHistoryResponse>> GetAllBySubscriptionIdAsync(string subscriptionId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<PriceHistoryResponse?> UpdateAsync(int id, UpdatePriceHistoryRequest req, string userId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<bool> DeleteAsync(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

6502f8feb848cecb15906d52820b8df73b19c0e4
lskld <theo.lillieskold@gmail.com>
2026-08-21 10:58:36 +0200

    change token service di to singleton

--- subwatch-api/Program.cs
+++ subwatch-api/Program.cs
@@ -56,7 +56,7 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddAuthorization();
 
-builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddSingleton<ITokenService, TokenService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ISubscriptionCategoryService, SubscriptionCategoryService>();
 builder.Services.AddScoped<ISubscriptionService, SubscriptionService>();

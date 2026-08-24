7af68c8e9b78c044ceb01b4a7b820a123c3498e2
lskld <theo.lillieskold@gmail.com>
2026-08-24 11:34:47 +0200

    create base of create subscription button

---
+++ subwatch-frontend/src/components/CreateSubscriptionButton.tsx
@@ -0,0 +1,11 @@
+import { IconPlus } from "@tabler/icons-react";
+
+export default function CreateSubscriptionButton() {
+    return (
+        <button
+            type="button"
+            className="flex gap-2 justify-center items-center bg-[#8FBF94] cursor-pointer text-sm w-40 h-8.25 xl:w-60 xl:h-12.5 xl:text-xl hover:opacity-75 border">
+            New subscription <IconPlus size={16} />
+        </button>
+    )
+}

2ddabac4648a24213c2c06c6519ca6681d2fefd5
lskld <theo.lillieskold@gmail.com>
2026-08-25 11:05:54 +0200

    add button borders

--- subwatch-frontend/src/components/LoginButton.tsx
+++ subwatch-frontend/src/components/LoginButton.tsx
@@ -2,13 +2,13 @@ import { NavLink } from "react-router-dom";
 
 export default function LoginButton() {
     return (
-        <NavLink to={"login"}>
-			<button
-				type="button"
-				className="h-10 w-30 bg-green-400 cursor-pointer hover:opacity-75"
-			>
-				Login
-			</button>
-		</NavLink>
-    )
+			<NavLink to={"login"}>
+				<button
+					type="button"
+					className="h-10 w-30 bg-green-400 cursor-pointer hover:opacity-75 border"
+				>
+					Login
+				</button>
+			</NavLink>
+		);
 }

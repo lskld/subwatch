82c30212ab7c7af156323f0965340e66f0cb20a2
lskld <theo.lillieskold@gmail.com>
2026-08-26 15:44:04 +0200

    remade axios api instance to one main instance instead and pass along jwt if it exists in localstorage

--- subwatch-frontend/src/lib/axios-instance.ts
+++ subwatch-frontend/src/lib/axios-instance.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
 
-export const authApi = axios.create({
-    baseURL: "http://localhost:5114/api/auth",
-})
+export const mainApi = axios.create({
+	baseURL: "http://localhost:5114/api/",
+});
+
+mainApi.interceptors.request.use((config) => {
+	const token = localStorage.getItem("jwt_token");
+
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+
+	return config;
+});

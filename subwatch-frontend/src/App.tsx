dd5fd845455f8290cddd843ba915c335e398b4ad
lskld <theo.lillieskold@gmail.com>
2026-08-26 11:56:03 +0200

    wrap all routes in the auth context provider

--- subwatch-frontend/src/App.tsx
+++ subwatch-frontend/src/App.tsx
@@ -4,20 +4,23 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { AuthProvider } from "./lib/auth-context";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<main>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="login" element={<Login />} />
-					<Route path="register" element={<Register />} />
-					<Route path="dashboard" element={<Dashboard />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</main>
-		</BrowserRouter>
+		<AuthProvider>
+			<BrowserRouter>
+				<main>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="login" element={<Login />} />
+						<Route path="register" element={<Register />} />
+						<Route path="dashboard" element={<Dashboard />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</main>
+			</BrowserRouter>
+		</AuthProvider>
 	);
 }
 

d5eb4475129e577cdba0db09c2ba561b80f5d031
lskld <theo.lillieskold@gmail.com>
2026-08-31 15:05:25 +0200

    Added a no filter option

--- subwatch-frontend/src/components/FilterDropdown.tsx
+++ subwatch-frontend/src/components/FilterDropdown.tsx
@@ -1,19 +1,28 @@
-import type { SubscriptionCategoryResponse } from "../api/types"
+import type { SubscriptionCategoryResponse } from "../api/types";
 
 type FilterProps = {
-    categories: SubscriptionCategoryResponse[];
-    onSelect: (category: SubscriptionCategoryResponse) => void;
-}
+	categories: SubscriptionCategoryResponse[];
+	onSelect: (category: SubscriptionCategoryResponse | null) => void;
+};
 
-export default function FilterDropdown({categories, onSelect}: FilterProps) {
-    return (
-        <div className="bg-white border flex flex-col text-left w-1/4 self-end">
-            {categories.map((category) => (
-                <button
-                    className="h-8 hover:bg-gray-400 cursor-pointer"
-                    onClick={() => onSelect(category)}
-                    key={category.id}>{category.title}</button>
-            ))}
-        </div>
-    )
+export default function FilterDropdown({ categories, onSelect }: FilterProps) {
+	return (
+		<div className="bg-white border flex flex-col text-left w-1/4 self-end">
+			<button
+				className="h-8 hover:bg-gray-400 cursor-pointer"
+				onClick={() => onSelect(null)}
+			>
+				No filter
+			</button>
+			{categories.map((category) => (
+				<button
+					className="h-8 hover:bg-gray-400 cursor-pointer"
+					onClick={() => onSelect(category)}
+					key={category.id}
+				>
+					{category.title}
+				</button>
+			))}
+		</div>
+	);
 }
